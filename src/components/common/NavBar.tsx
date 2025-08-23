947394e64a61f9a4b7db956df15996831c893e29
distribuidoraFenix <distribuidorafenixlasnieves@gmail.com>
2025-08-23 18:30:38 -0400

    prueba nuevo cambio

---
+++ src/components/common/NavBar.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Menu, Search } from "lucide-react";
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full flex items-center justify-between px-4 py-2 shadow-sm bg-violet-300">
+      
+      {/* Sección izquierda*/}
+      <div className="w-1/5 flex items-center">
+        <Menu className="w-8 h-8 text-gray-700 cursor-pointer md:ml-4" />
+      </div>
+
+      {/* Sección central (logo) */}
+      <div className="flex-1 flex justify-center">
+        <Image
+          src="/logos/fenixlogo.webp"
+          alt="Distribuidora Fenix"
+          width={36}
+          height={36}
+          priority
+        />
+      </div>
+
+      {/* Sección derecha */}
+      <div className="w-1/5 flex justify-end">
+        <Search className="w-8 h-8 text-gray-700 cursor-pointer md:mr-4" />
+      </div>
+    </nav>
+  );
+}
